ca7cb4b0fbedea2b440e027071bfd28a65ea43e4
adam kamissoko <adamkamissoko@gmail.com>
2023-11-07 15:14:21 +0400

    quizz_model generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'quizzes/show'
   get 'topics/index'
   get 'topics/show'
   devise_for :users
   root to: "subjects#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :subjects, only: %i[index show] do
-    resources :topics, only: %i[index show]
+    resources :topics, only: %i[index]
   end
+
+  resources :topics, only: %i[show] do
+    resources :quizzes, only: %i[index]
+  end
+
+  resources :quizzes, only: %i[show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_07_102642) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_07_105434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string "name"
+    t.bigint "topic_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_quizzes_on_topic_id"
+  end
 
   create_table "subjects", force: :cascade do |t|
     t.string "name"
@@ -40,5 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_07_102642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "quizzes", "topics"
   add_foreign_key "topics", "subjects"
 end
