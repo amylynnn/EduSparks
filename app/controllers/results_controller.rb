770543af6911d32d7c3402b993209c90b42b8820
vashist beedessy <benbeedessy15@gmail.com>
2023-11-14 17:43:13 +0400

    almost

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,7 +4,7 @@ class ResultsController < ApplicationController
     @user_answers = @result.user_answers
     @quiz = @result.quiz
     @questions = @quiz.questions
-    good_answers = @user_answers.map(&:answer).select(&:correct)
-    @percentage = (good_answers.count.to_f/ @questions.count.to_f).to_f * 100
+    @good_answers = @user_answers.select { |ua| ua.answer.correct if ua.answer.present? }
+    @percentage = (@good_answers.count.to_f / @questions.count.to_f) * 100
   end
 end
