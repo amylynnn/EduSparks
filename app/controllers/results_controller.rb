2407a59233e93f595dc80b0bf9c6079e1ae0db79
vashist beedessy <benbeedessy15@gmail.com>
2023-11-14 09:49:08 +0400

    not completed

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,10 @@
 class ResultsController < ApplicationController
   def show
+    @result = Result.find(params[:id])
+    @user_answers = @result.user_answers
+    @quiz = @result.quiz
+    @questions = @quiz.questions
+    good_answers = @user_answers.map(&:answer).select(&:correct)
+    @percentage = (good_answers.count.to_f/ @questions.count.to_f).to_f * 100
   end
 end
